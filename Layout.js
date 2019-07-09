b2c0477b2c13327896beb97f57aef63a5ebecb7e
Marce Ramirez <marceram87@gmail.com>
2019-07-09 15:28:41 +0200

    Demo finished

--- Layout.js
+++ Layout.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function Layout(root, style) {
+  this.root = root;
+  this.style = style;
+  this.elements = null;
+}
+
+Layout.prototype.generate = function() {
+  this.elements = `
+    <header id="site-header"></header>
+    <main id="site-main"></main>
+    <footer id="site-footer"></footer>
+  `;
+  this.render();
+  this.getContainers();
+}
+
+Layout.prototype.render = function() {
+  this.root.innerHTML = this.elements;
+}
+
+Layout.prototype.getContainers = function() {
+  this.header = document.querySelector('#site-header');
+  this.main = document.querySelector('#site-main');
+  this.footer = document.querySelector('#site-footer');
+}
+
--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 'use strict';
+
+function main() {
+  var ENTRY_POINT = '/';
+  var layoutInstance = null;
+  var navbarInstance = null;
+  var links = [
+    { name: 'Home', url: '/' }, 
+    { name: 'Movies', url: '/movies' }
+  ];
+  var rootElement = document.querySelector('#root');
+
+  generateLayout();
+  generateNavbar();
+  addListenersToNavbar();
+  activateRouter();
+
+  function generateLayout() {
+    layoutInstance = new Layout(rootElement);
+    layoutInstance.generate();
+  }
+
+  function generateNavbar() {
+    navbarInstance = new Navbar(layoutInstance.header, links);
+    navbarInstance.generate();
+  }
+
+  function activateRouter() {
+    routerInstance.buildDom(ENTRY_POINT, layoutInstance.main);
+  }
+
+  function addListenersToNavbar() {
+    var anchors = document.querySelectorAll('nav a');
+    anchors.forEach(function(anchor) {
+      anchor.addEventListener('click', changePage);
+    });
+  }
+
+  function changePage() {
+    var url = event.target.attributes.url.value;
+    routerInstance.buildDom(url, layoutInstance.main)
+  }
+}
+
+window.addEventListener('load', main);
